adfb2e589c466f432229d86d3a993c86e9a89527
trashbo4t <trashbo4t@protonmail.com>
2018-06-16 08:59:02 -0400

    Adding dependency script

---
+++ build-dep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing all dependencies"
+
+sudo apt-get install g++ git scons libportmidi-dev libopusfile-dev libshout-dev libtag1-dev libprotobuf-dev protobuf-compiler libusb-1.0-0-dev libfftw3-dev libmad0-dev portaudio19-dev libchromaprint-dev librubberband-dev libsqlite3-dev libid3tag0-dev libflac-dev libsndfile-dev libupower-glib-dev libavcodec-dev libavformat-dev libgl1-mesa-dev liblilv-dev libjack-dev libjack0 portaudio19-dev libfaad-dev libmp4v2-dev qt5-default libqt5opengl5-dev qtscript5-dev libqt5svg5-dev
+
+echo "Done"
